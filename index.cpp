176888283a3eb17a53635f054d27e1b4f3c4d091
飞天御剑流co <521luqiya@gmail.com>
2023-09-06 13:19:36 +0800

    class Box

--- index.cpp
+++ index.cpp
@@ -2,8 +2,45 @@
 
 #include <iostream>
 using namespace std;
-int main()
+
+class Box
 {
+public:
+  double length;
+  void setWidth(double wid);
+  double getWidth();
+
+private:
+  double width;
+};
+
+double Box::getWidth()
+{
+  return width;
+}
+
+void Box::setWidth(double wid)
+{
+  width = wid;
+}
+
+main()
+{
+
+  Box box;
+  box.length = 10;
+  box.setWidth(11.0);
+  cout << "width of box \n"
+       << box.getWidth() << endl;
   cout << "Hello, world!" << endl;
+
+  int a = 1;
+  cout << a << "\n";
+  for (int i = 1; i < 2; i++)
+  {
+    int a = 2;
+    cout << a << "\n";
+  }
+
   return 0;
 }
